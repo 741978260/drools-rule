636dd202dc60acc4d1b03332938ca8a390fc757c
cailin <cailin@vcredit.com>
2020-06-12 15:54:11 +0800

    drools规则文件编写

---
+++ src/main/java/com/mumu/drools/input/People.java
@@ -0,0 +1,38 @@
+package com.mumu.drools.input;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author cailin
+ * @since 2020/6/12
+ */
+public class People {
+    private Integer sex;
+    private String name;
+    private String drlType;
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDrlType() {
+        return drlType;
+    }
+
+    public void setDrlType(String drlType) {
+        this.drlType = drlType;
+    }
+}
